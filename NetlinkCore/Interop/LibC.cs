9d8001baae22eb7c0e3af18a62b12cddaa56c410
Vasyl Novikov <vanoviko@microsoft.com>
2025-12-28 23:37:57 +0000

    NetlinkSocket in progress

--- NetlinkCore/Interop/LibC.cs
+++ NetlinkCore/Interop/LibC.cs
@@ -2,18 +2,20 @@ using System.Runtime.InteropServices;
 
 namespace NetlinkCore.Interop;
 
-internal static partial class LibC
+internal static unsafe partial class LibC
 {
     private const string Lib = "libc";
 
     public const int AF_NETLINK = 16;
-    
+
     public const int SOCK_RAW = 3;
     public const int SOCK_CLOEXEC = 0x80000;
-    
+
     public const int NETLINK_ROUTE = 0;
     public const int NETLINK_NETFILTER = 12;
-    
+
+    public const int SOL_NETLINK = 270;
+
     public const int NETLINK_CAP_ACK = 10;
     public const int NETLINK_EXT_ACK = 11;
     public const int NETLINK_GET_STRICT_CHK = 12;
@@ -26,6 +28,10 @@ internal static partial class LibC
     [LibraryImport(Lib, EntryPoint = "socket", SetLastError = true)]
     public static partial int socket(int domain, int type, int protocol);
 
+    // int setsockopt(socklen_t optlen; int sockfd, int level, int optname, const void optval[optlen], socklen_t optlen);
+    [LibraryImport(Lib, EntryPoint = "setsockopt", SetLastError = true)]
+    public static partial int setsockopt(int sockfd, int level, int optname, void* optval, uint optlen);
+
     // int bind(int sockfd, const struct sockaddr_nl *addr, socklen_t addrlen);
     [LibraryImport(Lib, EntryPoint = "bind", SetLastError = true)]
     public static partial int bind(int sockfd, in sockaddr_nl addr, uint addrlen);
@@ -34,8 +40,8 @@ internal static partial class LibC
     public struct sockaddr_nl
     {
         public ushort nl_family; /* AF_NETLINK	*/
-        public ushort nl_pad; /* zero		*/
-        public uint nl_pid;		/* port ID	*/
-        public uint nl_groups;	/* multicast groups mask */
-    };
+        public ushort nl_pad;    /* zero		*/
+        public uint nl_pid;		 /* port ID	*/
+        public uint nl_groups;	 /* multicast groups mask */
+    }
 }
--- NetlinkCore/NetlinkSocket.cs
+++ NetlinkCore/NetlinkSocket.cs
@@ -6,32 +6,39 @@ using NetlinkCore.Interop;
 
 namespace NetlinkCore;
 
-public abstract class NetlinkSocket : IDisposable
+public abstract unsafe class NetlinkSocket : IDisposable
 {
     private readonly int _fd;
 
     [SuppressMessage("Usage", "CA1816: Dispose methods should call SuppressFinalize")]
-    protected unsafe NetlinkSocket(int protocol)
+    protected NetlinkSocket(int protocol)
     {
         _fd = LibC.socket(LibC.AF_NETLINK, LibC.SOCK_RAW | LibC.SOCK_CLOEXEC, protocol);
-        if (_fd == -1)
+        try
         {
-            GC.SuppressFinalize(this);
-            throw new Win32Exception();
-        }
+            if (_fd == -1)
+                throw new Win32Exception();
 
-        var address = new LibC.sockaddr_nl
-        {
-            nl_family = LibC.AF_NETLINK,
-            nl_pad = 0,
-            nl_pid = 0,
-            nl_groups = 0
-        };
-        if (LibC.bind(_fd, address, (uint)sizeof(LibC.sockaddr_nl)) == -1)
+            SetOption(LibC.NETLINK_CAP_ACK, 1);
+            SetOption(LibC.NETLINK_EXT_ACK, 1);
+            SetOption(LibC.NETLINK_GET_STRICT_CHK, 1);
+
+            var address = new LibC.sockaddr_nl
+            {
+                nl_family = LibC.AF_NETLINK,
+                nl_pad = 0,
+                nl_pid = 0,
+                nl_groups = 0
+            };
+            if (LibC.bind(_fd, address, (uint)sizeof(LibC.sockaddr_nl)) == -1)
+                throw new Win32Exception();
+        }
+        catch
         {
             GC.SuppressFinalize(this);
-            LibC.close(_fd);
-            throw new Win32Exception();
+            if (_fd != -1)
+                LibC.close(_fd);
+            throw;
         }
     }
 
@@ -42,4 +49,10 @@ public abstract class NetlinkSocket : IDisposable
     }
 
     ~NetlinkSocket() => LibC.close(_fd);
+
+    private void SetOption(int option, int value)
+    {
+        if (LibC.setsockopt(_fd, LibC.SOL_NETLINK, option, &value, sizeof(int)) == -1)
+            throw new Win32Exception();
+    }
 }
