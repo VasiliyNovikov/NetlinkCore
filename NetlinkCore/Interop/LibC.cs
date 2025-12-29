df9f490f4e0bdcc7c493cbf1d2b84815b0f03e4e
Vasyl Novikov <vanoviko@microsoft.com>
2025-12-29 19:22:04 +0000

    Switch to LinuxCore

--- NetlinkCore/Interop/LibC.cs
+++ NetlinkCore/Interop/LibC.cs
@@ -2,39 +2,14 @@ using System.Runtime.InteropServices;
 
 namespace NetlinkCore.Interop;
 
-internal static unsafe partial class LibC
+internal static class LibC
 {
-    private const string Lib = "libc";
-
-    public const int AF_NETLINK = 16;
-
-    public const int SOCK_RAW = 3;
-    public const int SOCK_CLOEXEC = 0x80000;
-
     public const int NETLINK_ROUTE = 0;
     public const int NETLINK_NETFILTER = 12;
-
-    public const int SOL_NETLINK = 270;
 
     public const int NETLINK_CAP_ACK = 10;
     public const int NETLINK_EXT_ACK = 11;
     public const int NETLINK_GET_STRICT_CHK = 12;
-
-    // int close(int fd);
-    [LibraryImport(Lib, EntryPoint = "close", SetLastError = true)]
-    public static partial int close(int fd);
-
-    // int socket(int domain, int type, int protocol);
-    [LibraryImport(Lib, EntryPoint = "socket", SetLastError = true)]
-    public static partial int socket(int domain, int type, int protocol);
-
-    // int setsockopt(socklen_t optlen; int sockfd, int level, int optname, const void optval[optlen], socklen_t optlen);
-    [LibraryImport(Lib, EntryPoint = "setsockopt", SetLastError = true)]
-    public static partial int setsockopt(int sockfd, int level, int optname, void* optval, uint optlen);
-
-    // int bind(int sockfd, const struct sockaddr_nl *addr, socklen_t addrlen);
-    [LibraryImport(Lib, EntryPoint = "bind", SetLastError = true)]
-    public static partial int bind(int sockfd, in sockaddr_nl addr, uint addrlen);
 
     [StructLayout(LayoutKind.Sequential)]
     public struct sockaddr_nl
--- NetlinkCore/NetlinkSocket.cs
+++ NetlinkCore/NetlinkSocket.cs
@@ -1,58 +1,28 @@
-﻿using System;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Net.Sockets;
+
+using LinuxCore;
 
 using NetlinkCore.Interop;
 
 namespace NetlinkCore;
 
-public abstract unsafe class NetlinkSocket : IDisposable
+public abstract class NetlinkSocket : LinuxSocketBase
 {
-    private readonly int _fd;
-
-    [SuppressMessage("Usage", "CA1816: Dispose methods should call SuppressFinalize")]
-    protected NetlinkSocket(int protocol)
+    protected NetlinkSocket(int protocol) : base(LinuxAddressFamily.Netlink, LinuxSocketType.Raw, (ProtocolType)protocol)
     {
-        _fd = LibC.socket(LibC.AF_NETLINK, LibC.SOCK_RAW | LibC.SOCK_CLOEXEC, protocol);
-        try
+        SetOption(LibC.NETLINK_CAP_ACK, 1);
+        SetOption(LibC.NETLINK_EXT_ACK, 1);
+        SetOption(LibC.NETLINK_GET_STRICT_CHK, 1);
+
+        var address = new LibC.sockaddr_nl
         {
-            if (_fd == -1)
-                throw new Win32Exception();
-
-            SetOption(LibC.NETLINK_CAP_ACK, 1);
-            SetOption(LibC.NETLINK_EXT_ACK, 1);
-            SetOption(LibC.NETLINK_GET_STRICT_CHK, 1);
-
-            var address = new LibC.sockaddr_nl
-            {
-                nl_family = LibC.AF_NETLINK,
-                nl_pad = 0,
-                nl_pid = 0,
-                nl_groups = 0
-            };
-            if (LibC.bind(_fd, address, (uint)sizeof(LibC.sockaddr_nl)) == -1)
-                throw new Win32Exception();
-        }
-        catch
-        {
-            GC.SuppressFinalize(this);
-            if (_fd != -1)
-                LibC.close(_fd);
-            throw;
-        }
+            nl_family = (ushort)LinuxAddressFamily.Netlink,
+            nl_pad = 0,
+            nl_pid = 0,
+            nl_groups = 0
+        };
+        Bind(address);
     }
 
-    public void Dispose()
-    {
-        GC.SuppressFinalize(this);
-        LibC.close(_fd);
-    }
-
-    ~NetlinkSocket() => LibC.close(_fd);
-
-    private void SetOption(int option, int value)
-    {
-        if (LibC.setsockopt(_fd, LibC.SOL_NETLINK, option, &value, sizeof(int)) == -1)
-            throw new Win32Exception();
-    }
+    private void SetOption(int option, int value) => base.SetOption(LinuxSocketOptionLevel.Netlink, option, value);
 }
