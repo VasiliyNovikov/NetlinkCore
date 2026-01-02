9c1a571fc3c02ecfbe568fe6456a843165252314
Vasyl Novikov <vanoviko@microsoft.com>
2026-01-02 17:55:26 +0000

    Link collection test

--- NetlinkCore.Tests/NetlinkSocketTests.cs
+++ NetlinkCore.Tests/NetlinkSocketTests.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 using LinuxCore;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -25,6 +27,18 @@ public class NetlinkSocketTests
         Assert.AreEqual("lo", link.Name);
         Assert.IsGreaterThan(0, link.IfIndex);
         Assert.AreEqual(default(MACAddress), link.MacAddress);
+    }
+
+    [TestMethod]
+    public void RouteNetlinkSocket_GetLinks()
+    {
+        using var socket = new RouteNetlinkSocket();
+        var links = socket.GetLinks();
+        Assert.IsGreaterThan(1, links.Length);
+        var lo = links.Single(l => l.Name == "lo");
+        Assert.AreEqual("lo", lo.Name);
+        Assert.IsGreaterThan(0, lo.IfIndex);
+        Assert.AreEqual(default(MACAddress), lo.MacAddress);
     }
 
     [TestMethod]
--- NetlinkCore/RouteNetlinkSocket.cs
+++ NetlinkCore/RouteNetlinkSocket.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 using NetlinkCore.Interop.Route;
@@ -22,27 +23,53 @@ public sealed class RouteNetlinkSocket() : NetlinkSocket(NetlinkFamily.Route)
         };
         messageWriter.Attributes.Write(ifinfomsg_attrs.IFLA_IFNAME, name);
         Send(messageWriter.Written);
-        var receivedCount = Receive(buffer);
-        var received = (ReadOnlySpan<byte>)buffer[..receivedCount];
+        var receivedLength = Receive(buffer);
+        var received = (ReadOnlySpan<byte>)buffer[..receivedLength];
         foreach (var message in new RouteNetlinkMessageCollection<ifinfomsg, ifinfomsg_type, ifinfomsg_attrs>(received))
-        {
-            var ifIndex = message.Header.ifi_index;
-            MACAddress? macAddress = null;
-            foreach (var attribute in message.Attributes)
-            {
-                Console.Error.WriteLine($"Attribute: {attribute.Name}, Length: {attribute.Data.Length}");
-                switch (attribute.Name)
-                {
-                    case ifinfomsg_attrs.IFLA_IFNAME:
-                        name = attribute.AsString();
-                        break;
-                    case ifinfomsg_attrs.IFLA_ADDRESS:
-                        macAddress = attribute.AsValue<MACAddress>();
-                        break;
-                }
-            }
-            return new Link(ifIndex, name, macAddress);
-        }
+            if (message.Type == ifinfomsg_type.RTM_NEWLINK)
+                return ParseLink(message);
         throw new InvalidOperationException($"Link with name '{name}' not found.");
+    }
+
+    public Link[] GetLinks()
+    {
+        Unsafe.SkipInit<NetlinkBuffer>(out var buffer);
+        var messageWriter = new RouteNetlinkMessageWriter<ifinfomsg, ifinfomsg_type, ifinfomsg_attrs>(buffer)
+        {
+            Type = ifinfomsg_type.RTM_GETLINK,
+            Flags = NetlinkMessageFlags.Request | NetlinkMessageFlags.Dump,
+            PortId = PortId,
+            Header = default
+        };
+        Send(messageWriter.Written);
+        var receivedLength = Receive(buffer);
+        var received = (ReadOnlySpan<byte>)buffer[..receivedLength];
+        var links = new List<Link>();
+        foreach (var message in new RouteNetlinkMessageCollection<ifinfomsg, ifinfomsg_type, ifinfomsg_attrs>(received))
+            if (message.Type == ifinfomsg_type.RTM_NEWLINK)
+                links.Add(ParseLink(message));
+        return [.. links];
+    }
+
+    private static Link ParseLink(RouteNetlinkMessage<ifinfomsg, ifinfomsg_type, ifinfomsg_attrs> message)
+    {
+        var ifIndex = message.Header.ifi_index;
+        string? name = null;
+        MACAddress? macAddress = null;
+        foreach (var attribute in message.Attributes)
+        {
+            switch (attribute.Name)
+            {
+                case ifinfomsg_attrs.IFLA_IFNAME:
+                    name = attribute.AsString();
+                    break;
+                case ifinfomsg_attrs.IFLA_ADDRESS:
+                    macAddress = attribute.AsValue<MACAddress>();
+                    break;
+            }
+        }
+        return name is null
+            ? throw new InvalidOperationException($"Link with index '{ifIndex}' is missing a name attribute.")
+            : new Link(ifIndex, name, macAddress);
     }
 }
