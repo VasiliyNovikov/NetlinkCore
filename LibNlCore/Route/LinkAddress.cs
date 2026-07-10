8438ee33c27e7b7be5215beab1c2ae09bc9da62e
Vasyl Novikov <vanoviko@microsoft.com>
2026-07-10 11:39:11 +0000

    Code review fix

--- LibNlCore/Route/LinkAddress.cs
+++ LibNlCore/Route/LinkAddress.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Net;
 
 namespace LibNlCore.Route;
 
-public sealed class LinkAddress(IPAddress address, byte prefixLength, bool noDad = false) : RouteAddress(address, prefixLength)
+public sealed class LinkAddress : RouteAddress
 {
-    public bool NoDad => noDad;
+    public bool NoDad { get; }
+
+    public LinkAddress(IPAddress address, byte prefixLength, bool noDad = false)
+        : base(address, prefixLength)
+    {
+        ArgumentOutOfRangeException.ThrowIfZero(prefixLength);
+        NoDad = noDad;
+    }
 
     public new static LinkAddress Parse(string addressString)
     {
